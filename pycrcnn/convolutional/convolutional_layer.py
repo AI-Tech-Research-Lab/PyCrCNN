0dff30608b6c29efa02749e89b0464d8a58f71d5
Alessandro Falcetta <alessandro.falcetta@mail.polimi.it>
2019-10-23 12:05:24 +0200

    Implemented Padding in AveragePool Moved utility func in functional.padding Added more tests

--- pycrcnn/convolutional/convolutional_layer.py
+++ pycrcnn/convolutional/convolutional_layer.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+from pycrcnn.functional.padding import apply_padding
 from ..crypto import crypto as c
 
 
@@ -52,37 +54,6 @@ class ConvolutionalLayer:
             return np.array([[layer + bias for layer, bias in zip(image, self.bias)] for image in result])
         else:
             return result
-
-
-def apply_padding(t, padding):
-    """Execute a padding operation given a batch of images in the form
-        [n_image, n_layer, y, x]
-       After the execution, the result will be in the form
-        [n_image, n_layer, y+padding, x+padding]
-       The element in the new rows/column will be zero.
-       Due to Pyfhel limits, a sum/product between two PyPtxt can't be done.
-       This leads to the need of having a PyCtxt which has to be zero if decrypted: this is done by
-       subtracting an arbitrary value to itself.
-
-    Parameters
-    ----------
-    t: np.array( dtype=PyCtxt )
-        Encrypted image to execute the padding on, in the form
-        [n_images, n_layer, y, x]
-    padding: (int, int)
-
-    Returns
-    -------
-    result : np.array( dtype=PyCtxt )
-        Encrypted result of the padding, in the form
-        [n_images, n_layer, y+padding, x+padding]
-    """
-
-    y_p = padding[0]
-    x_p = padding[1]
-    zero = t[0][0][y_p+1][x_p+1] - t[0][0][y_p+1][x_p+1]
-    result = [[np.pad(mat, ((y_p, y_p), (x_p, x_p)), 'constant', constant_values=zero) for mat in layer] for layer in t]
-    return result
 
 
 def convolute2d(image, filter_matrix, stride):
--- pycrcnn/functional/average_pool.py
+++ pycrcnn/functional/average_pool.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from pycrcnn.functional.padding import apply_padding
+
 
 class AveragePoolLayer:
     """
@@ -24,12 +26,14 @@ class AveragePoolLayer:
             [n_images, n_layers, y, x]
         using kernel size and strides of the layer.
     """
-    def __init__(self, HE, kernel_size, stride):
+    def __init__(self, HE, kernel_size, stride=(1, 1), padding=(0, 0)):
         self.HE = HE
         self.kernel_size = kernel_size
         self.stride = stride
+        self.padding = padding
 
     def __call__(self, t):
+        t = apply_padding(t, self.padding)
         return np.array([[_avg(self.HE, layer, self.kernel_size, self.stride) for layer in image] for image in t])
 
 
@@ -44,27 +48,33 @@ def _avg(HE, image, kernel_size, stride):
         image : np.array( dtype=PyCtxt )
             Encrypted image to execute the pooling, in the form
             [y, x]
-        kernel_size : int
-            size of the square kernel
-        stride : int
-
+        kernel_size : (int, int)
+            size of the kernel (y, x)
+        stride : (int, int)
+            stride (y, x)
         Returns
         -------
         result : np.array( dtype=PtCtxt )
             Encrypted result of the pooling, in the form
             [y, x]
         """
+    x_s = stride[1]
+    y_s = stride[0]
+
+    x_k = kernel_size[1]
+    y_k = kernel_size[0]
+
     x_d = len(image[0])
     y_d = len(image)
 
-    x_o = ((x_d - kernel_size) // stride) + 1
-    y_o = ((y_d - kernel_size) // stride) + 1
+    x_o = ((x_d - x_k) // x_s) + 1
+    y_o = ((y_d - y_k) // y_s) + 1
 
-    denominator = HE.encodeFrac(1 / (kernel_size * kernel_size))
+    denominator = HE.encodeFrac(1 / (x_k * y_k))
 
     def get_submatrix(matrix, x, y):
-        index_row = y * stride
-        index_column = x * stride
-        return matrix[index_row: index_row + kernel_size, index_column: index_column + kernel_size]
+        index_row = y * y_s
+        index_column = x * x_s
+        return matrix[index_row: index_row + y_k, index_column: index_column + x_k]
 
     return [[np.sum(get_submatrix(image, x, y)) * denominator for x in range(0, x_o)] for y in range(0, y_o)]
