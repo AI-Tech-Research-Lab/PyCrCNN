de654e6ae06a0ffe8ca245d82220f0abab9092bc
Alessandro Falcetta <alessandro.falcetta@mail.polimi.it>
2019-11-07 11:13:33 +0100

    Added time debug in server

--- pycrcnn/network/server/main.py
+++ pycrcnn/network/server/main.py
@@ -1,14 +1,17 @@
 import glob
 import tempfile
+import time
 import zipfile
 import os
 from multiprocessing import Process
 
 import jsonpickle
+import torch
 from Pyfhel import Pyfhel
 from flask import Flask, request, send_file
 import numpy as np
 
+from pycrcnn.net_builder.encoded_net_builder import build_from_pytorch
 from pycrcnn.network.server.executer import perform_computation
 
 app = Flask(__name__)
@@ -40,12 +43,16 @@ def handle_request():
         - Answer the post request with this file
     """
 
-    def compute():
+    def compute_debug():
+        print("DEBUG: Request arrived...")
         print("DEBUG: Unzipping and collecting encryption parameters...")
+        start_time = time.process_time()
         parameters = jsonpickle.decode(request.files["parameters"].read())
         request.files["input"].save(os.path.join(zip_temp_dir.name, "input.zip"))
         zf = zipfile.ZipFile(os.path.join(zip_temp_dir.name, "input.zip"), mode='r')
         zf.extractall(request_temp_dir.name)
+
+        time_unzip = time.process_time() - start_time
 
         HE = Pyfhel()
         HE.contextGen(m=parameters["encryption_parameters"][0]["m"],
@@ -63,6 +70,78 @@ def handle_request():
         enc_image = np.array([[[[HE.encryptFrac(0) for i in range(0, columns)] for j in range(0, rows)]
                             for k in range(0, layers)] for z in range(0, images)])
 
+        def populate_enc_image(image, layer, row, col):
+            name = str(image) + "-" + str(layer) + "-" + str(row) + "-" + str(col)
+            enc_image[image][layer][row][col].load(os.path.join(request_temp_dir.name, name), "float")
+
+        [[[[populate_enc_image(image, layer, row, col) for col in range(0, columns)] for
+          row in range (0, rows)] for layer in range(0, layers)] for image in range(0, images)]
+
+        time_reconstructing = time.process_time() - time_unzip
+
+        print("DEBUG: Perform the computation on the image...")
+        layers = parameters["layers"]
+
+        if parameters["net"] == "MNIST":
+            plain_net = torch.load("./mnist.pt")
+            plain_net.eval()
+
+        # Choose how many layers encode
+        plain_net = plain_net[min(layers):max(layers) + 1]
+        encoded_net = build_from_pytorch(HE, plain_net)
+
+        time_encoding = time.process_time() - time_unzip
+
+        for layer in encoded_net:
+            enc_image = layer(enc_image)
+        result = enc_image
+
+        time_computation = time.process_time() - time_encoding
+
+        print("DEBUG: Zipping the response...")
+        zf = zipfile.ZipFile(os.path.join(zip_temp_dir.name, "answer.zip"), mode='w')
+
+        for image in range(0, len(result)):
+            for layer in range(0, len(result[image])):
+                for row in range(0, len(result[image][layer])):
+                    for col in range(0, len(result[image][layer][row])):
+                        name = str(image) + "-" + str(layer) + "-" + str(row) + "-" + str(col)
+                        result[image][layer][row][col].save(os.path.join(answer_temp_dir.name, name))
+                        zf.write(os.path.join(answer_temp_dir.name, name), compress_type=zipfile.ZIP_DEFLATED,
+                                 arcname=name)
+        zf.close()
+        time_zipping = time.process_time() - time_computation
+
+        print("DEBUG: Sending the response...")
+        total_time = (time_unzip + time_reconstructing + time_encoding + time_computation + time_zipping) / 100
+        print("Time to unzip: ", '{:6.3f}'.format(time_unzip), " seconds, % = ", '{:6.3f}'.format(time_unzip / total_time))
+        print("Time to reconstruct image: ", '{:6.3f}'.format(time_reconstructing), " seconds, % = ", '{:6.3f}'.format(time_reconstructing / total_time))
+        print("Time to encode layers: ", '{:6.3f}'.format(time_encoding), " seconds, % = ", '{:6.3f}'.format(time_encoding / total_time))
+        print("Time to compute: ", '{:6.3f}'.format(time_computation), " seconds, % = ", '{:6.3f}'.format(time_computation / total_time))
+        print("Time to zip result: ", '{:6.3f}'.format(time_zipping), " seconds, % = ", '{:6.3f}'.format(time_zipping / total_time))
+
+    def compute():
+
+        parameters = jsonpickle.decode(request.files["parameters"].read())
+        request.files["input"].save(os.path.join(zip_temp_dir.name, "input.zip"))
+        zf = zipfile.ZipFile(os.path.join(zip_temp_dir.name, "input.zip"), mode='r')
+        zf.extractall(request_temp_dir.name)
+
+        HE = Pyfhel()
+        HE.contextGen(m=parameters["encryption_parameters"][0]["m"],
+                      p=parameters["encryption_parameters"][0]["p"],
+                      sec=parameters["encryption_parameters"][0]["sec"],
+                      base=parameters["encryption_parameters"][0]["base"])
+        HE.keyGen()
+        HE.relinKeyGen(20, 5)
+
+        input_files = sorted([os.path.basename(f) for f in glob.glob(request_temp_dir.name + "/*")])
+        numbers = [file.split("-") for file in input_files]
+        images, layers, rows, columns = max([[int(x)+1 for x in item] for item in numbers])
+
+        enc_image = np.array([[[[HE.encryptFrac(0) for i in range(0, columns)] for j in range(0, rows)]
+                            for k in range(0, layers)] for z in range(0, images)])
+
         for image in range(0, images):
             for layer in range(0, layers):
                 for row in range(0, rows):
@@ -70,10 +149,8 @@ def handle_request():
                         name = str(image) + "-" + str(layer) + "-" + str(row) + "-" + str(col)
                         enc_image[image][layer][row][col].load(os.path.join(request_temp_dir.name, name), "float")
 
-        print("DEBUG: Perform the computation on the image...")
         result = perform_computation(HE, enc_image, parameters["net"], parameters["layers"])
 
-        print("DEBUG: Zipping the response...")
         zf = zipfile.ZipFile(os.path.join(zip_temp_dir.name, "answer.zip"), mode='w')
 
         for image in range(0, len(result)):
@@ -86,15 +163,15 @@ def handle_request():
 
         zf.close()
 
-        print("DEBUG: Sending the response...")
-
-    print("DEBUG: Request arrived...")
     request_temp_dir = tempfile.TemporaryDirectory()
     answer_temp_dir = tempfile.TemporaryDirectory()
     zip_temp_dir = tempfile.TemporaryDirectory()
 
     # Executes the computation on Pyfhel objects in a separate process to avoid memory unreferenced
-    p = Process(target=compute)
+    if app.debug:
+        p = Process(target=compute_debug)
+    else:
+        p = Process(target=compute)
     p.start()
     p.join()
 
@@ -102,4 +179,4 @@ def handle_request():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
