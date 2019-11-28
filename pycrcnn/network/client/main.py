3ea783931c7bd8e907bd0342b3cae81b8258d340
Alessandro Falcetta <alessandro.falcetta@mail.polimi.it>
2019-11-28 10:47:53 +0100

    Removed the zip stuff for a new encoding/decoding mechanism. Now the POST and the POST'response contains only JSONs.

--- pycrcnn/network/client/main.py
+++ pycrcnn/network/client/main.py
@@ -1,7 +1,5 @@
-import glob
-import os
+import base64
 import tempfile
-import zipfile
 
 import jsonpickle
 import numpy as np
@@ -15,6 +13,7 @@ from pycrcnn.crypto.crypto import decrypt_matrix, encrypt_matrix
 
 def main():
     """
+
     This function executes a POST request given files:
         - parameters: JSON file
         - input_image: JSON file
@@ -27,10 +26,10 @@ def main():
         - Decode the input image from the JSON file in a numpy array
         - Creates the Pyfhel object, generate the keys
         - Encrypt the image
-        - Create a zip file containing all the ciphertext in the matrix, saved one by one in a file with a meaningful
-          name, i.e "0-1-2-3" is the file containing the ciphertext of image 0, layer 1, row 2, column 3
-        - Send the post request with the parameters and the zip file
-        - After receiving the response, do the opposite of pass 4, i.e de-serialize the answer zip file in a
+        - Create an array with all the b64 value of every ciphertext's saved file
+        - Append these data to the parameters
+        - Send the post request with the JSON containing parameters and data
+        - After receiving the response, do the opposite of pass 4, i.e de-serialize the answer JSON in a
           numpy matrix of ciphertexts
         - After the result is reconstructed in a suitable numpy array of ciphertext, decrypt it to obtain the plain
           result
@@ -38,9 +37,24 @@ def main():
           the result is correct (for debug purposes)
     """
 
-    request_temp_dir = tempfile.TemporaryDirectory()
-    answer_temp_dir = tempfile.TemporaryDirectory()
-    zip_temp_dir = tempfile.TemporaryDirectory()
+    jsonpickle.set_encoder_options('json', indent=4, sort_keys=False)
+    temp_file = tempfile.NamedTemporaryFile()
+
+    def encode_ciphertext(c):
+        with (open(temp_file.name, "w+b")) as f:
+            c.save(temp_file.name)
+            bc = f.read()
+            b64 = str(base64.b64encode(bc))[2:-1]
+            return b64
+
+    def decode_ciphertext(b64):
+        with (open(temp_file.name, "w+b")) as f:
+            x = bytes(b64, encoding='utf-8')
+            x = base64.decodebytes(x)
+            f.write(x)
+            c = HE.encryptFrac(0)
+            c.load(temp_file.name, "float")
+            return c
 
     with open("./input_image.json", "rb") as f:
         input_image = jsonpickle.decode(f.read())
@@ -60,63 +74,36 @@ def main():
     print("DEBUG: Encrypting matrix...")
     enc_image = encrypt_matrix(HE, input_image)
 
-    print("DEBUG: Zipping...")
-    zf = zipfile.ZipFile(os.path.join(zip_temp_dir.name, 'input.zip'), mode='w')
+    print("DEBUG: Encode for JSON...")
+    data = [[[[encode_ciphertext(value) for value in row]
+              for row in column]
+             for column in layer]
+            for layer in enc_image]
 
-    for image in range(0, len(enc_image)):
-        for layer in range(0, len(enc_image[image])):
-            for row in range(0, len(enc_image[image][layer])):
-                for col in range(0, len(enc_image[image][layer][row])):
-                    name = str(image) + "-" + str(layer) + "-" + str(row) + "-" + str(col)
-                    enc_image[image][layer][row][col].save(request_temp_dir.name + "/" + name)
-                    zf.write(os.path.join(request_temp_dir.name, name), compress_type=zipfile.ZIP_DEFLATED, arcname=name)
+    print("DEBUG: Sending request...")
+    payload = parameters
+    payload["data"] = data
 
-    zf.close()
-    print("DEBUG: Zip size=", os.path.getsize(os.path.join(zip_temp_dir.name, 'input.zip'))/(1024*1024), "MB")
+    res = requests.post(address, json=jsonpickle.encode(payload))
+    enc_result = jsonpickle.decode(res.content)["data"]
 
-    files = {
-        'parameters': open("./parameters.json", "rb"),
-        'input': open(os.path.join(zip_temp_dir.name, 'input.zip'), 'rb')
-    }
-
-    print("DEBUG: Sending the request...")
-    response = requests.post(address, files=files)
-
-    print("DEBUG: Response received, saving...")
-    # open(os.path.join(zip_temp_dir.name, 'answer.zip'), 'wb').write(response.content)
-    open(os.path.join(zip_temp_dir.name, 'answer.zip'), 'wb').write(jsonpickle.decode(response.content))
-
-    print("DEBUG: Extracting response...")
-    zf = zipfile.ZipFile(os.path.join(zip_temp_dir.name, 'answer.zip'), mode='r')
-    zf.extractall(answer_temp_dir.name)
-
-    print("DEBUG: Reconstructing answer...")
-    answer_files = sorted([os.path.basename(f) for f in glob.glob(answer_temp_dir.name + "/*")])
-    numbers = [file.split("-") for file in answer_files]
-    images, layers, rows, columns = max([[int(x) + 1 for x in item] for item in numbers])
-
-    result = np.array([[[[HE.encryptFrac(0) for i in range(0, columns)] for j in range(0, rows)]
-                        for k in range(0, layers)] for z in range(0, images)])
-
-    for image in range(0, images):
-        for layer in range(0, layers):
-            for row in range(0, rows):
-                for col in range(0, columns):
-                    name = str(image) + "-" + str(layer) + "-" + str(row) + "-" + str(col)
-                    result[image][layer][row][col].load(os.path.join(answer_temp_dir.name, name), "float")
+    enc_result = np.array([[[[decode_ciphertext(value) for value in row]
+                             for row in column]
+                            for column in layer]
+                           for layer in enc_result])
 
     print("DEBUG: Print answer decrypted...")
-    dec_result = decrypt_matrix(HE, result)
+    dec_result = decrypt_matrix(HE, enc_result)
     print(dec_result)
-
-    # EXTRA DEBUG TO CHECK THE RESULTS
-    print("DEBUG: Plain results...")
-    plain_net = torch.load("./mnist.pt")
-    plain_net.eval()
-
-    plain_net = plain_net[0:4]
-    results_plain = plain_net(torch.tensor(input_image))
-    print(results_plain)
+    #
+    # # EXTRA DEBUG TO CHECK THE RESULTS
+    # print("DEBUG: Plain results...")
+    # plain_net = torch.load("./mnist.pt")
+    # plain_net.eval()
+    #
+    # plain_net = plain_net[0:4]
+    # results_plain = plain_net(torch.tensor(input_image))
+    # print(results_plain)
 
 
 if __name__ == '__main__':
--- pycrcnn/network/server/main.py
+++ pycrcnn/network/server/main.py
@@ -1,17 +1,12 @@
-import glob
+import base64
+import multiprocessing
 import tempfile
-import time
-import zipfile
-import os
-from multiprocessing import Process
 
 import jsonpickle
-import torch
 from Pyfhel import Pyfhel
-from flask import Flask, request, send_file
+from flask import Flask, request
 import numpy as np
 
-from pycrcnn.net_builder.encoded_net_builder import build_from_pytorch
 from pycrcnn.network.server.executer import perform_computation
 
 app = Flask(__name__)
@@ -25,77 +20,83 @@ def index():
 @app.route('/compute', methods=['POST'])
 def handle_request():
     """
-    This function receives a POST request with two files:
-        - parameters: JSON file
-        - input: zip file
+    This function receives a POST request with a JSON containing:
+        - parameters
+        - input
 
     Parameters contains both the encryption parameters requested, as p, m, sec level and base and the model requested,
     with the layers on which the computation is requested (for example: MNIST, layers from 0 to 3)
 
+    Input contains the array of encoded PyCtxt. In this context, encoded means they are the dump of every ciphertext's
+    file encoded in b64.
+
     Due to limitations in the serialization of Pyfhel objects, the following algorithm is executed:
 
-        - Extract all the files in the input file in a temporary directory
-        - Reconstruct the encrypted image in a numpy array: every extracted file matches a position in the matrix,
-          i.e "0-1-2-3" is the file containing the ciphertext of image 0, layer 1, row 2, column 3
+        - Reconstruct the encrypted input, decoding every b64 value in the array data
         - After the image is reconstructed in a suitable numpy array of ciphertext, perform the encrypted computation
           with the layers encoded with the aforementioned parameters
-        - Once the result is ready, serialize that in a zip file doing the opposite process of point 1
-        - Answer the post request with this file
+        - Once the result is ready, serialize all the values of the answer in b64
+        - Answer the post request with the JSON containing those values
     """
 
-    def compute(parameters, input_zip_file):
+    def compute(payload, return_dict):
+        temp_file = tempfile.NamedTemporaryFile()
 
-        parameters = jsonpickle.decode(parameters.read())
+        def encode_ciphertext(c):
+            with (open(temp_file.name, "w+b")) as f:
+                c.save(temp_file.name)
+                bc = f.read()
+                b64 = str(base64.b64encode(bc))[2:-1]
+                return b64
 
-        request_temp_dir = tempfile.TemporaryDirectory()
-        answer_temp_dir = tempfile.TemporaryDirectory()
-        zip_temp_dir = tempfile.TemporaryDirectory()
+        def decode_ciphertext(b64):
+            with (open(temp_file.name, "w+b")) as f:
+                x = bytes(b64, encoding='utf-8')
+                x = base64.decodebytes(x)
+                f.write(x)
+                c = HE.encryptFrac(0)
+                c.load(temp_file.name, "float")
+                return c
 
-        input_zip_file.save(os.path.join(zip_temp_dir.name, "input.zip"))
-        zf = zipfile.ZipFile(os.path.join(zip_temp_dir.name, "input.zip"), mode='r')
-        zf.extractall(request_temp_dir.name)
+        payload = jsonpickle.decode(payload)
+        encryption_parameters = payload["encryption_parameters"]
+        data = payload["data"]
 
         HE = Pyfhel()
-        HE.contextGen(m=parameters["encryption_parameters"][0]["m"],
-                      p=parameters["encryption_parameters"][0]["p"],
-                      sec=parameters["encryption_parameters"][0]["sec"],
-                      base=parameters["encryption_parameters"][0]["base"])
+        HE.contextGen(m=encryption_parameters[0]["m"],
+                      p=encryption_parameters[0]["p"],
+                      sec=encryption_parameters[0]["sec"],
+                      base=encryption_parameters[0]["base"])
         HE.keyGen()
         HE.relinKeyGen(20, 5)
 
-        input_files = sorted([os.path.basename(f) for f in glob.glob(request_temp_dir.name + "/*")])
-        numbers = [file.split("-") for file in input_files]
-        images, layers, rows, columns = max([[int(x)+1 for x in item] for item in numbers])
+        enc_image = np.array([[[[decode_ciphertext(value) for value in row]
+                    for row in column]
+                    for column in layer]
+                    for layer in data])
 
-        enc_image = np.array([[[[HE.encryptFrac(0) for i in range(0, columns)] for j in range(0, rows)]
-                            for k in range(0, layers)] for z in range(0, images)])
+        result = perform_computation(HE, enc_image, payload["net"], payload["layers"])
 
-        for image in range(0, images):
-            for layer in range(0, layers):
-                for row in range(0, rows):
-                    for col in range(0, columns):
-                        name = str(image) + "-" + str(layer) + "-" + str(row) + "-" + str(col)
-                        enc_image[image][layer][row][col].load(os.path.join(request_temp_dir.name, name), "float")
+        to_encode = [[[[encode_ciphertext(value) for value in row]
+                    for row in column]
+                    for column in layer]
+                    for layer in result]
 
-        result = perform_computation(HE, enc_image, parameters["net"], parameters["layers"])
+        answer = {
+            "data": to_encode
+        }
+        return_dict["answer"] = jsonpickle.encode(answer)
 
-        zip_temp_dir = tempfile.TemporaryDirectory()
-        zf = zipfile.ZipFile(os.path.join(zip_temp_dir.name, "answer.zip"), mode='w')
 
-        for image in range(0, len(result)):
-            for layer in range(0, len(result[image])):
-                for row in range(0, len(result[image][layer])):
-                    for col in range(0, len(result[image][layer][row])):
-                        name = str(image) + "-" + str(layer) + "-" + str(row) + "-" + str(col)
-                        result[image][layer][row][col].save(os.path.join(answer_temp_dir.name, name))
-                        zf.write(os.path.join(answer_temp_dir.name, name), compress_type=zipfile.ZIP_DEFLATED,
-                                 arcname=name)
-        zf.close()
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
 
-        return jsonpickle.encode(open(os.path.join(zip_temp_dir.name, "answer.zip"), "rb").read())
+    p = multiprocessing.Process(target=compute, args=(request.json, return_dict))
+    p.start()
+    p.join()
 
     response = app.response_class(
-        response=compute(request.files["parameters"], request.files["input"]),
+        response=return_dict["answer"],
         mimetype='application/json'
     )
     return response
